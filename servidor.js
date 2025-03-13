178211c30f7a332c580683900b45997149bd6ae1
fabricionetto <agostinhonetto73@gmail.com>
2025-03-13 11:18:40 -0300

    Rotas

--- servidor.js
+++ servidor.js
@@ -3,9 +3,21 @@ const meuhost = '127.0.0.1';
 const porta=8000;
 
 const requisicoes = ((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Meu Primeiro servidor\n');
+    const url = req.url;
+    const metodo = req.method
+    if(url === '/' && metodo === 'GET'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Meu Primeiro servidor texto\n');
+    } else if(url === '/pagina' && metodo === 'GET'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<html><body><h1>This is HTML</h1><h2>Pagina Html</h2></body></html>');
+    } else {
+        res.setHeader('Content-Type', 'text/plain');
+        res.writeHead(404);
+        res.end('Nao tem pohha nenhuma\n');
+    }
+    
 });
 
 const servidor = http.createServer(requisicoes);
